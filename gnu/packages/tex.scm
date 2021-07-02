6edad02df21ed49cca8ed132ce4f436704e9d6da
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:07:08 +0200

    gnu: texlive-biber: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/tex.scm (texlive-biber)[inputs]: Add 'bash-minimal'.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Modified-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I50b81e2d41f9e1a800de3f43ba6b50bbaf4eb471

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -56925,7 +56925,8 @@ documents as well as DVI output.")
            perl-module-build
            perl-test-differences))
     (inputs
-     (list perl
+     (list bash-minimal
+           perl
            perl-autovivification
            perl-class-accessor
            perl-data-dump
