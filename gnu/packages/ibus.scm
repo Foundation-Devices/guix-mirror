4c1a1de4f34178eb87c76fb728ba62a6eaca1e60
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:07:12 +0200

    gnu: ibus: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/ibus.scm
    (ibus-libpinyin)[inputs]: Add 'bash-minimal'.
    (ibus-anthy)[inputs]: Likewise.
    (ibus-libhangul)[inputs]: Likewise.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I50ef48da68dd048aba243b013a7b66e504697dc7

--- gnu/packages/ibus.scm
+++ gnu/packages/ibus.scm
@@ -45,6 +45,7 @@
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages boost)
   #:use-module (gnu packages check)
   #:use-module (gnu packages cmake)
@@ -317,7 +318,8 @@ may also simplify input method development.")
                                    "/share/ibus-libpinyin/setup/")
                    ,(getenv "GI_TYPELIB_PATH")))))))))
     (inputs
-     (list ibus
+     (list bash-minimal
+           ibus
            libpinyin
            bdb
            sqlite
@@ -438,7 +440,8 @@ Chinese pinyin input methods.")
            util-linux                   ;for getopt
            xorg-server-for-tests))
     (inputs
-     (list anthy
+     (list bash-minimal
+           anthy
            gtk+
            ibus
            gobject-introspection
@@ -907,7 +910,8 @@ hanja dictionary and small hangul character classification.")
        ("gettext" ,gettext-minimal)
        ("glib:bin" ,glib "bin")))
     (inputs
-     (list ibus
+     (list bash-minimal
+           ibus
            glib
            python-pygobject
            gtk+
