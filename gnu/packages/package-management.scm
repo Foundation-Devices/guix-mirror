7b50b31624b7f058f263feb2854096ad35b4abf5
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-10-09 12:03:43 -0400

    gnu: guix-build-coordinator: Import the correct set of modules.
    * gnu/packages/package-management.scm (guix-build-oordinator) [arguments]
    <modules>: Replace %default-gnu-imported-modules with %default-gnu-modules.
    
    Change-Id: Iceeb14b22359b626aa3b0712a3d701ed7211c4be

--- gnu/packages/package-management.scm
+++ gnu/packages/package-management.scm
@@ -1529,7 +1529,7 @@ environments.")
        (list
         #:modules `(((guix build guile-build-system)
                      #:select (target-guile-effective-version))
-                    ,@%default-gnu-imported-modules)
+                    ,@%default-gnu-modules)
         #:imported-modules `((guix build guile-build-system)
                              ,@%default-gnu-imported-modules)
         #:phases
