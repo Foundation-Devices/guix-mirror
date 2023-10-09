4df2d69e66a0d94b9f5f346a9a67080a052f65ad
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-10-09 11:45:07 -0400

    gnu: mozjs: Import the correct set of modules.
    * gnu/packages/gnuzilla.scm (mozjs) [arguments]
    <modules>: Replace %default-gnu-imported-modules with %default-gnu-modules.
    
    Change-Id: I2e5f210a95aa04ffeb3046b9bd5738cac13ce377

--- gnu/packages/gnuzilla.scm
+++ gnu/packages/gnuzilla.scm
@@ -117,7 +117,7 @@
      (list
       #:imported-modules %cargo-utils-modules ;for `generate-all-checksums'
       #:modules `((guix build cargo-utils)
-                  ,@%default-gnu-imported-modules)
+                  ,@%default-gnu-modules)
       #:test-target "check-jstests"
       #:configure-flags
       #~(list
