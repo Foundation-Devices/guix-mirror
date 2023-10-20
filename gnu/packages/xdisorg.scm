f4cbc7fc0d284b73cbf0d06974b79c030ee87077
Efraim Flashner <efraim@flashner.co.il>
2023-10-20 15:58:37 +0300

    gnu: mtdev: Fix cross-compiling to aarch64-linux.
    * gnu/packages/xdisorg.scm (mtdev)[arguments]: When cross-compiling to
    aarch64-linux update the config.{guess,sub} also.
    [native-inputs]: When cross-compiling to aarch64 add config.

--- gnu/packages/xdisorg.scm
+++ gnu/packages/xdisorg.scm
@@ -801,7 +801,8 @@ and Matrox.")
     (arguments
      `(#:configure-flags
        '("--disable-static")
-       ,@(if (and (target-riscv64?)
+       ,@(if (and (or (target-riscv64?)
+                      (target-aarch64?))
                   (%current-target-system))
            `(#:phases
              (modify-phases %standard-phases
@@ -816,7 +817,8 @@ and Matrox.")
                              '("config.guess" "config.sub"))))))
            '())))
     (native-inputs
-     (if (and (target-riscv64?)
+     (if (and (or (target-riscv64?)
+                  (target-aarch64?))
               (%current-target-system))
        (list config)
        '()))
