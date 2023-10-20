34922d610685136e6040e3a83fd81772c04fd1ba
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-10-20 01:07:23 -0400

    gnu: shorah: Add bash-minimal to inputs.
    * gnu/packages/bioinformatics.scm (shorah)
    [inputs]: Add bash-minimal.
    
    Change-Id: Ibe428a0ec32a5acf7e70269376aaf16e97507d51

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -11130,7 +11130,8 @@ data.  It also generates basic statistics for your sequences.")
              (when tests?
                (invoke "make" "check")))))))
     (inputs
-     (list boost
+     (list bash-minimal
+           boost
            htslib
            python
            python-biopython
