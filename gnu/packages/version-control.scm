7e053e02ee610b10eb1cea7663ccd4ff6058ba34
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-10-19 23:53:05 -0400

    gnu: myrepos: Add bash-minimal to inputs.
    * gnu/packages/version-control.scm (myrepos)
    [inputs]: Add bash-minimal.
    
    Change-Id: I97feb4f5d8ad03fcde2bd8579c5a133d89904c8a

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -2664,10 +2664,10 @@ modification time.")
                                                     "/lib/perl5/site_perl"))
                          '("perl-encode-locale" "perl-http-date"
                            "perl-http-message" "perl-html-parser" "perl-libwww"
-                           "perl-uri" "perl-try-tiny"))))
-               #t))))))
+                           "perl-uri" "perl-try-tiny"))))))))))
     (inputs
-     (list perl
+     (list bash-minimal                 ;for wrap-program
+           perl
            perl-encode-locale
            perl-html-parser
            perl-http-date
