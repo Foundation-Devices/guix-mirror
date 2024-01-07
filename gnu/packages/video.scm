9400188b2b57cea2f3efc998a2f3b24769db04b7
Andy Tai <atai@atai.org>
2024-01-06 17:42:54 -0800

    gnu: ffmpeg: add svt-av1 support
    * gnu/packages/video.scm (ffmpeg): Add support for svt-av1
    [inputs]: Add svt-av1
    [arguments] (configure-flags): Add "--enable-libsvtav1"
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: Idb0166e943a4c69a56b68964fd6f3439b16f1903

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -1713,6 +1713,7 @@ operate properly.")
             soxr
             speex
             srt
+            svt-av1
             twolame
             vidstab
             x265
@@ -1801,6 +1802,7 @@ operate properly.")
          "--enable-libsoxr"
          "--enable-libspeex"
          "--enable-libsrt"
+         "--enable-libsvtav1"
          "--enable-libtheora"
          "--enable-libtwolame"
          "--enable-libvidstab"
