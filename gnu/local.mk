546e0accd0df237134b0bffdd144773c0a0f6fff
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-01-21 18:16:16 +0100

    gnu: gnumach: Update to 1.8-0.2556fde.
    * gnu/packages/hurd.scm (gnumach-headers): Update to latest master,
    2556fdece900d67529d5eda01f1bdaae4ffe96b0.
    * gnu/packages/patches/gnumach-fix-i686-linux-build.patch,
    gnu/packages/patches/gnumach-fix-task_info.patch,
    gnu/packages/patches/gnumach-support-noide.patch: Remove files.
    * gnu/local.mk (dist_patch_DATA): Remove them.
    (gnumach)[source]: Do not use them.
    
    Change-Id: Ifc6d15f7889854cd85698f4133f70d2046ca4bc8

--- gnu/local.mk
+++ gnu/local.mk
@@ -1375,9 +1375,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/gnome-settings-daemon-gc.patch		\
   %D%/packages/patches/gnome-session-support-elogind.patch	\
   %D%/packages/patches/gnome-tweaks-search-paths.patch		\
-  %D%/packages/patches/gnumach-fix-i686-linux-build.patch	\
-  %D%/packages/patches/gnumach-fix-task_info.patch		\
-  %D%/packages/patches/gnumach-support-noide.patch		\
   %D%/packages/patches/gnupg-default-pinentry.patch		\
   %D%/packages/patches/gnupg-1-build-with-gcc10.patch		\
   %D%/packages/patches/gnutls-skip-trust-store-test.patch	\
