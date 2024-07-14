601859e239106798c700cda41ff34541d0346953
Liliana Marie Prikler <liliana.prikler@gmail.com>
2024-07-14 12:56:46 +0200

    gnu: Make vte-with-gtk4 the default vte.
    * gnu/packages/gnome.scm (vte): Use flags from…
    (vte-with-gtk4): … this.  Delete variable.
    (vte/gtk+3): New variable.
    (gnome-color-manager, vinagre, gnome-console, gnome-terminal)
    (terminator, gnome-boxes): Adjust accordingly.
    * gnu/packages/guile-xyz.scm (nomad): Likewise.
    * gnu/packages/lxde.scm (lxterminal): Likewise.
    * gnu/packages/mate.scm (mate-terminal): Likewise.
    * gnu/packages/spice.scm (virt-viewer): Likewise.
    * gnu/packages/sugar.scm (sugar-terminal-activity): Likewise.
    * gnu/packages/terminals.scm (tilda, sakura, xiate, tilix): Likewise.
    * gnu/packages/virtualization.scm (virt-manager): Likewise.
    * gnu/packages/vnc.scm (remmina): Likewise.
    * gnu/packages/xfce.scm (xfce4-terminal): Likewise.

--- gnu/packages/virtualization.scm
+++ gnu/packages/virtualization.scm
@@ -1769,7 +1769,7 @@ virtualization library.")
            python-requests
            qemu
            spice-gtk
-           vte))
+           vte/gtk+-3))
     (native-inputs
      (list `(,glib "bin")               ; glib-compile-schemas
            gobject-introspection
