Millak/guix

commit 53bec65c51c3f2b7e318db4dc7a5c3a09ad00214

gnu: gsl: Provide static library in separate output.
* gnu/packages/maths.scm (gsl)[outputs]: New field.
[arguments]: Remove configure-flag disabling static library.  Add
make-flag to produce PIC output.  Add a phase to move the static
libraries into the static output.

Change-Id: I4c5dddb8d384f4e2f82122b239da44350bacf57a

gnu/packages/maths.scm
@@ -725,10 +725,11 @@ precision floating point numbers.")
               (sha256
                (base32
                 "0jxkxrnpys2j3rh8bzx0bmnh4w6xm28jd57rgxsjp0s863agpc6w"))))
+    (outputs '("out" "static"))
     (build-system gnu-build-system)
     (arguments
      (let ((system (%current-system)))
-       `(#:configure-flags (list "--disable-static") ;halves package size
+       `(#:make-flags (list "CFLAGS=-fPIC")
          #:phases
          (modify-phases %standard-phases
            ,@(cond
@@ -774,7 +775,16 @@ precision floating point numbers.")
                        (("gsl_ieee_env_setup.*" all)
                         (string-append "exit (77);\n" all)))))))
 
-              (else '()))))))
+              (else '()))
+           (add-after 'install 'move-static-library
+             (lambda* (#:key outputs #:allow-other-keys)
+               (let ((static (string-append (assoc-ref outputs "static") "/lib/"))
+                     (out    (string-append (assoc-ref outputs "out") "/lib/")))
+                 (mkdir-p static)
+                 (rename-file (string-append out "libgsl.a")
+                              (string-append static "libgsl.a"))
+                 (rename-file (string-append out "libgslcblas.a")
+                              (string-append static "libgslcblas.a")))))))))
     (native-inputs
      (if (and (target-riscv64?)
               (%current-target-system))

Foundation-Devices/guix-mirror

commit 85a2d861d815ffee24f109c2c5fe2cdba64e23ca

gnu: gsl: Build with ‘-O2 -g’ unconditionally.
Fixes a regression introduced in
53bec65c51c3f2b7e318db4dc7a5c3a09ad00214 that would cause gsl to be
built without optimizations and without debugging symbols (equivalent to
“-O0 -g0”).

* gnu/packages/maths.scm (gsl)[arguments]: Remove #:make-flags.

Change-Id: I5297e3602bffbb7655d655622cc3d7595f82b25a

gnu/packages/maths.scm
@@ -728,17 +728,7 @@ precision floating point numbers.")
     (outputs '("out" "static" "debug"))
     (build-system gnu-build-system)
     (arguments
-     (list ;; FIXME: Setting CFLAGS=-fPIC is not only unnecessary, it's also
-           ;; harmful because it removes the default '-O2 -g', meaning that the
-           ;; library ends up being compiled as -O0.  Consequently, some
-           ;; numerical tests fail, notably on i686-linux.  TODO: Remove
-           ;; 'CFLAGS=-fPIC' for all systems and revisit or remove
-           ;; 'disable-failing-tests' phases accordingly.
-           #:make-flags (if (and (not (%current-target-system))
-                                 (string=? (%current-system) "i686-linux"))
-                            #~'()
-                            #~(list "CFLAGS=-fPIC"))
-           #:phases
+     (list #:phases
            #~(modify-phases %standard-phases
                #$@(cond
                    ((and (target-riscv64?)